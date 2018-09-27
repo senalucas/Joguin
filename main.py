94bcf885111413c880cefbca539452f4ba4d28f9
Lucas Sena <email.lucassena@gmail.com>
2018-09-27 11:26:13 -0300

    Função de Movimentação

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame,sys,os,objetos
+import pygame,sys,os,objetos,movimentacao
 
 largura=800
 altura=600
@@ -10,60 +10,25 @@ def jogo():
     pygame.display.set_caption("Maçãs Forever 2.0")
     background = pygame.image.load("Images/Fundo/fundo.png")
 
-    caminhoDaFonte = os.path.join("fontes","VCR_OSD_MONO.ttf")
-    branco = (255,255,255)
-    placar = pygame.font.Font(caminhoDaFonte,25)
-    mostrarPlacar = placar.render("Pontuação: ",True,branco)
-
     p1 = objetos.Coisas("Images/Elementos/parede1.png",400,80)
     p2 = objetos.Coisas("Images/Elementos/parede2.png",84,295)
     p3 = objetos.Coisas("Images/Elementos/parede3.png",399,520)
     p4 = objetos.Coisas("Images/Elementos/parede4.png",715,294)
+
+    grupo = pygame.sprite.Group()
+    grupo.add(p1)
+    grupo.add(p2)
+    grupo.add(p3)
+    grupo.add(p4)
     p = objetos.Coisas("Images/Elementos/portao1.png",largura/2,520)
     j = objetos.Personagem(largura/2,altura/2)
-    
-    jogo = True
 
-    while jogo:
+    while True:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        keys = pygame.key.get_pressed()
-        if(pygame.sprite.collide_rect(j,p1)) :
-            j.rect = j.rect.move(0,+(j.andando))
-        elif(pygame.sprite.collide_rect(j,p2)):
-            j.rect = j.rect.move(+(j.andando),0)
-        elif(pygame.sprite.collide_rect(j,p3)):
-            j.rect = j.rect.move(0,-(j.andando))
-        elif(pygame.sprite.collide_rect(j,p4)):
-            j.rect = j.rect.move(-(j.andando),0)
-        else:
-            clock = pygame.time.get_ticks()
-            if keys[pygame.K_w]:
-                if clock % 2 == 0:
-                    j.Char = pygame.image.load("Images/Personagem/bonecodecostas1.png")
-                else:
-                    j.Char = pygame.image.load("Images/Personagem/bonecodecostas2.png")
-                j.rect = j.rect.move(0,-(j.andando))
-            if keys[pygame.K_s]:
-                if clock % 2 == 0:
-                    j.Char = pygame.image.load("Images/Personagem/bonecodefrente1.png")
-                else:
-                    j.Char = pygame.image.load("Images/Personagem/bonecodefrente2.png")
-                j.rect = j.rect.move(0,+(j.andando))
-            if keys[pygame.K_a]:
-                if clock % 2 == 0:
-                    j.Char = pygame.image.load("Images/Personagem/bonecopraesquerda1.png")
-                else:
-                    j.Char = pygame.image.load("Images/Personagem/bonecopraesquerda2.png")
-                j.rect = j.rect.move(-(j.andando),0)
-            if keys[pygame.K_d]:
-                if clock % 2 == 0:
-                    j.Char = pygame.image.load("Images/Personagem/bonecopradireita1.png")
-                else:
-                    j.Char = pygame.image.load("Images/Personagem/bonecopradireita2.png")
-                j.rect = j.rect.move(+(j.andando),0)
+        j.movimentacao()
 
         tela.blit(background,(0,0))
         p1.mostrar(tela)
--- objetos.py
+++ objetos.py
@@ -1,5 +1,7 @@
-import pygame
+import pygame,os
+
 pygame.init()
+
 largura=800
 altura=600
 
@@ -14,15 +16,54 @@ class Personagem(pygame.sprite.Sprite):
         self.rect.centery = self.y
 
         self.andando = 1
+
     def mostrar(self, tela):
         tela.blit(self.Char,self.rect)
+
+    def movimentacao(self):
+        clock = pygame.time.get_ticks()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            if clock % 2 == 0:
+                self.Char = pygame.image.load("Images/Personagem/bonecodecostas1.png")
+            else:
+                self.Char = pygame.image.load("Images/Personagem/bonecodecostas2.png")
+            self.rect = self.rect.move(0,-(self.andando))
+        if keys[pygame.K_s]:
+            if clock % 2 == 0:
+                self.Char = pygame.image.load("Images/Personagem/bonecodefrente1.png")
+            else:
+                self.Char = pygame.image.load("Images/Personagem/bonecodefrente2.png")
+            self.rect = self.rect.move(0,+(self.andando))
+        if keys[pygame.K_a]:
+            if clock % 2 == 0:
+                self.Char = pygame.image.load("Images/Personagem/bonecopraesquerda1.png")
+            else:
+                self.Char = pygame.image.load("Images/Personagem/bonecopraesquerda2.png")
+            self.rect = self.rect.move(-(self.andando),0)
+        if keys[pygame.K_d]:
+            if clock % 2 == 0:
+                self.Char = pygame.image.load("Images/Personagem/bonecopradireita1.png")
+            else:
+                self.Char = pygame.image.load("Images/Personagem/bonecopradireita2.png")
+            self.rect = self.rect.move(+(self.andando),0)
 
 class Coisas(pygame.sprite.Sprite):
     def __init__(self, caminho, x,y):
         pygame.sprite.Sprite.__init__(self)
-        self.Muro = pygame.image.load(caminho).convert_alpha()
-        self.rect = self.Muro.get_rect()
+        self.Coisa = pygame.image.load(caminho).convert_alpha()
+        self.rect = self.Coisa.get_rect()
         self.rect.centerx = x
         self.rect.centery = y
+
     def mostrar(self,tela):
-        tela.blit(self.Muro,self.rect)
+        tela.blit(self.Coisa,self.rect)
+
+class Textos():
+    def __init__(self, texto,tam,r,g,b):
+        self.caminho = os.path.join("fontes","VCR_OSD_MONO.ttf")
+        self.texto = pygame.font.Font(self.caminho,tam)
+        self.cor = (r,g,b)
+        self.mostrarTexto = self.texto.render(texto,True,self.cor)
+    def mostrarTextoNaTela(self,tela,x,y):
+        tela.blit(self.mostrarTexto,(x,y))
