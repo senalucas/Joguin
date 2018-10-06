7aaed8238d376090a6e5d3607cb8906a60118fa9
Lucas Sena <email.lucassena@gmail.com>
2018-10-06 15:07:47 -0300

    Adicionando caminho da imagem

--- definicoes.py
+++ definicoes.py
@@ -11,6 +11,7 @@ class Diretorios():
         self.porta1 = "Images/Elementos/portao1.png"
         self.porta2 = "Images/Elementos/portao2.png"
         self.fundo = "Images/Fundo/fundo.png"
+        self.fundoInicial = "Images/Fundo/fundoTelaInicial.png"
         self.macav = "Images/Elementos/maca_vermelha.png"
         self.macad = "Images/Elementos/maca_dourada.png"
         self.ghostd = "Images/Elementos/ghost1.png"        
