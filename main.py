84dc0dcb89f85e08c9e4d64229d055dab988d8b6
Lucas Sena <email.lucassena@gmail.com>
2018-10-06 15:08:03 -0300

    Colocando Menu da tela inicial

--- main.py
+++ main.py
@@ -3,6 +3,36 @@ import pygame,sys,os,objetos,char,definicoes,levels.level1
 largura=800
 altura=600
 
+def menu():
+    pygame.init()
+
+    tela = pygame.display.set_mode([largura,altura])
+    images = definicoes.Diretorios()        
+    pygame.display.set_caption("Maçãs Forever 2.0")
+    background = pygame.image.load(images.fundoInicial)
+    portao = objetos.Coisas(images.porta1,(largura/2)-15,altura-225)
+    t = objetos.Textos("Maçãs Forever 2.0",42,255,255,255)
+    iniciar = objetos.Textos("Iniciar",30,255,255,255)  
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            for x in range(320,448):
+                for y in range(420,451):
+                    if ((pygame.mouse.get_pos()[0] == x) and (pygame.mouse.get_pos()[1] == y)):
+                        if pygame.mouse.get_pressed()[0]:
+                            jogo()                
+            
+
+        tela.blit(background,(0,0))
+        iniciar.mostrarTextoNaTela(tela,320,420)
+        t.mostrarTextoNaTela(tela,((largura/4)),((altura)-altura*0.9))
+        portao.mostrar(tela)
+        
+        pygame.display.update()
+
+
 def jogo():
     pygame.init()
 
@@ -35,4 +65,4 @@ def jogo():
         nivel1.movimaca(j,tela)
         pygame.display.update()
 
-jogo()
+menu()
